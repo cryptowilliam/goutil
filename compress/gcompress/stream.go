8e242efbaf191c4a409a789c567a47a6bf1c2715
cryptowilliam <cryptowilliam@hotmail.com>
2021-11-09 21:13:16 +0800

    refactor stream

--- compress/gcompress/stream.go
+++ compress/gcompress/stream.go
@@ -16,30 +16,30 @@ import (
 )
 
 type (
-	WriterFlusher interface {
+	WriteFlusher interface {
 		io.Writer
 		gio.Flusher
 	}
 
-	CompIOStream struct {
+	CompReadWriteCloser struct {
 		algo  CompAlgo
 		param *CompParam
 		rwc   io.ReadWriteCloser
 		r     io.Reader
-		w     WriterFlusher
+		w     WriteFlusher
 	}
 
-	CompNetStream struct {
-		conn     net.Conn
-		ioStream *CompIOStream
+	CompStream struct {
+		conn            net.Conn
+		readWriteCloser *CompReadWriteCloser
 	}
 )
 
-func (c *CompIOStream) Read(p []byte) (n int, err error) {
+func (c *CompReadWriteCloser) Read(p []byte) (n int, err error) {
 	return c.r.Read(p)
 }
 
-func (c *CompIOStream) Write(p []byte) (n int, err error) {
+func (c *CompReadWriteCloser) Write(p []byte) (n int, err error) {
 	n, err = c.w.Write(p)
 	if err != nil {
 		return 0, err
@@ -51,12 +51,12 @@ func (c *CompIOStream) Write(p []byte) (n int, err error) {
 	return n, nil
 }
 
-func (c *CompIOStream) Close() error {
+func (c *CompReadWriteCloser) Close() error {
 	return c.rwc.Close()
 }
 
-func NewIOStream(compAlgo CompAlgo, param *CompParam, rwc io.ReadWriteCloser) (*CompIOStream, error) {
-	rst := new(CompIOStream)
+func NewCompReadWriteCloser(compAlgo CompAlgo, param *CompParam, rwc io.ReadWriteCloser) (*CompReadWriteCloser, error) {
+	rst := new(CompReadWriteCloser)
 	rst.algo = compAlgo
 	if param != nil {
 		*rst.param = *param
@@ -120,45 +120,45 @@ func NewIOStream(compAlgo CompAlgo, param *CompParam, rwc io.ReadWriteCloser) (*
 	return rst, nil
 }
 
-func (c *CompNetStream) Read(p []byte) (n int, err error) {
-	return c.ioStream.Read(p)
+func (c *CompStream) Read(p []byte) (n int, err error) {
+	return c.readWriteCloser.Read(p)
 }
 
-func (c *CompNetStream) Write(p []byte) (n int, err error) {
-	return c.ioStream.Write(p)
+func (c *CompStream) Write(p []byte) (n int, err error) {
+	return c.readWriteCloser.Write(p)
 }
 
-func (c *CompNetStream) Close() error {
-	return c.ioStream.Close()
+func (c *CompStream) Close() error {
+	return c.readWriteCloser.Close()
 }
 
-func (c *CompNetStream) LocalAddr() net.Addr {
+func (c *CompStream) LocalAddr() net.Addr {
 	return c.conn.LocalAddr()
 }
 
-func (c *CompNetStream) RemoteAddr() net.Addr {
+func (c *CompStream) RemoteAddr() net.Addr {
 	return c.conn.RemoteAddr()
 }
 
-func (c *CompNetStream) SetDeadline(t time.Time) error {
+func (c *CompStream) SetDeadline(t time.Time) error {
 	return c.conn.SetDeadline(t)
 }
 
-func (c *CompNetStream) SetReadDeadline(t time.Time) error {
+func (c *CompStream) SetReadDeadline(t time.Time) error {
 	return c.conn.SetReadDeadline(t)
 }
 
-func (c *CompNetStream) SetWriteDeadline(t time.Time) error {
+func (c *CompStream) SetWriteDeadline(t time.Time) error {
 	return c.conn.SetWriteDeadline(t)
 }
 
-func NewNetStream(compAlgo CompAlgo, param *CompParam, conn net.Conn) (*CompNetStream, error) {
-	rst := new(CompNetStream)
-	ioStream, err := NewIOStream(compAlgo, param, conn)
+func NewCompStream(compAlgo CompAlgo, param *CompParam, conn net.Conn) (*CompStream, error) {
+	rst := new(CompStream)
+	readWriteCloser, err := NewCompReadWriteCloser(compAlgo, param, conn)
 	if err != nil {
 		return nil, err
 	}
-	rst.ioStream = ioStream
+	rst.readWriteCloser = readWriteCloser
 	rst.conn = conn
 	return rst, nil
 }
