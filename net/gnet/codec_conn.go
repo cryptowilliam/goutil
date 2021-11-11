a8758dc077e49634ae45fec879afd4da0d56a3dd
cryptowilliam <cryptowilliam@hotmail.com>
2021-11-11 21:38:07 +0800

    Update codec_conn.go

--- net/gnet/codec_conn.go
+++ net/gnet/codec_conn.go
@@ -7,8 +7,9 @@ import (
 )
 
 type (
-	// CodecConn implements net.Conn, used to compress/decompress network connection.
-	// For example, transferring log files or JSON files over the Internet,
+	// CodecConn is a generic stream-oriented network connection that can be compressed and decompressed.
+	// It implements net.Conn interface. It is used to compress/decompress network connection stream,
+	// for example, when transferring log files or JSON files over the Internet,
 	// adding a compression algorithm like snappy can greatly improve the efficiency of data transfer.
 	CodecConn struct {
 		conn                 net.Conn           // original net.Conn used to access LocalAddr/RemoteAddr/SetDeadline/Close...
@@ -18,42 +19,50 @@ type (
 
 // NewCodecConn create CodecConn with original connection and codec io.ReadWriteCloser.
 // Note: you should implement data compress/decompress at `codecReadWriteCloser`
-func NewCodecConn(conn net.Conn, codecReadWriteCloser io.ReadWriteCloser) *CodecConn {
+func NewCodecConn(conn net.Conn, codec io.ReadWriteCloser) *CodecConn {
 	rst := new(CodecConn)
 	rst.conn = conn
-	rst.codecReadWriteCloser = codecReadWriteCloser
+	rst.codecReadWriteCloser = codec
 	return rst
 }
 
+// Read implements net.Conn.
 func (c *CodecConn) Read(p []byte) (n int, err error) {
 	return c.codecReadWriteCloser.Read(p)
 }
 
+// Write implements net.Conn.
 func (c *CodecConn) Write(p []byte) (n int, err error) {
 	return c.codecReadWriteCloser.Write(p)
 }
 
+// Close implements net.Conn.
 func (c *CodecConn) Close() error {
 	_ = c.codecReadWriteCloser.Close()
 	return c.conn.Close()
 }
 
+// LocalAddr implements net.Conn.
 func (c *CodecConn) LocalAddr() net.Addr {
 	return c.conn.LocalAddr()
 }
 
+// RemoteAddr implements net.Conn.
 func (c *CodecConn) RemoteAddr() net.Addr {
 	return c.conn.RemoteAddr()
 }
 
+// SetDeadline implements net.Conn.
 func (c *CodecConn) SetDeadline(t time.Time) error {
 	return c.conn.SetDeadline(t)
 }
 
+// SetReadDeadline implements net.Conn.
 func (c *CodecConn) SetReadDeadline(t time.Time) error {
 	return c.conn.SetReadDeadline(t)
 }
 
+// SetWriteDeadline implements net.Conn.
 func (c *CodecConn) SetWriteDeadline(t time.Time) error {
 	return c.conn.SetWriteDeadline(t)
 }
