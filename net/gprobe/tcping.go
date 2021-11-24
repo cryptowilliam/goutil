397a1dbc489febf1475d1fda8910d0a3ec9d3085
cryptowilliam <cryptowilliam@hotmail.com>
2021-11-24 15:32:53 +0800

    modify TCPing func

--- net/gprobe/tcping.go
+++ net/gprobe/tcping.go
@@ -8,24 +8,34 @@ import (
 	"time"
 )
 
-func TcpingOnline(host string, port int, timeout time.Duration) (opened bool, err error) {
+func TCPing(host string, port int, timeout time.Duration) (opened bool, duration time.Duration, err error) {
 	if !gaddr.IsValidPort(port) {
-		return false, gerrors.Errorf("Invalid port " + gnum.ToString(port))
+		return false, 0, gerrors.Errorf("Invalid port " + gnum.ToString(port))
 	}
 
-	ip, _, err := gaddr.ParseHostAddrOnline(host)
-	if err != nil {
-		return false, err
+	ip := ""
+	if gaddr.IsIPString(host) {
+		ip = host
+	} else {
+		ipArr, err := gaddr.LookupIP(host)
+		if err != nil {
+			return false, 0, err
+		}
+		if len(ipArr) == 0 {
+			return false, 0, gerrors.New("ip addresses length is zero")
+		}
+		ip = ipArr[0].String()
 	}
 
-	conn, err := net.DialTimeout("tcp", ip.String()+":"+gnum.ToString(port), timeout)
+	startTime := time.Now()
+	conn, err := net.DialTimeout("tcp", ip+":"+gnum.ToString(port), timeout)
 	defer func() {
 		if conn != nil {
 			conn.Close()
 		}
 	}()
 	if err != nil {
-		return false, nil // Maybe Closed
+		return false, time.Now().Sub(startTime), nil // Maybe Closed
 	}
-	return true, nil // Opened
+	return true, time.Now().Sub(startTime), nil // Opened
 }
