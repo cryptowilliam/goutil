c5290d2a2a9373519c355bf8ab014e2422c3e9f8
cryptowilliam <cryptowilliam@hotmail.com>
2021-11-24 22:27:36 +0800

    add Listen and Serve func

--- net/gsocks5/socks5_server.go
+++ net/gsocks5/socks5_server.go
@@ -3,16 +3,21 @@ package gsocks5
 import (
 	"github.com/cryptowilliam/goutil/net/gnet"
 	socks5internal "github.com/cryptowilliam/goutil/net/gsocks5/socks5internal"
+	"net"
 )
 
 type (
 	Server struct {
+		lis net.Listener
+		srv *socks5internal.Server
 		listenAddr string
 		dialer     gnet.DialWithCtxFunc
 	}
 )
 
 // NewServer create new socks5 server.
+// For now, non-tcp socks5 proxy server is not necessary, so there is no "network" param.
+// listenAddr example: "127.0.0.1:8000"
 func NewServer(listenAddr string) *Server {
 	return &Server{listenAddr: listenAddr}
 }
@@ -42,4 +47,25 @@ func (s *Server) ListenAndServe() error {
 		return err
 	}
 	return nil
+}
+
+// Listen start a tcp listener.
+func (s *Server) Listen() error {
+	// Create a SOCKS5 server
+	conf := &socks5internal.Config{}
+	if s.dialer != nil {
+		conf.Dial = s.dialer
+	}
+	err := error(nil)
+	s.srv, err = socks5internal.New(conf)
+	if err != nil {
+		return err
+	}
+	s.lis, err = net.Listen("tcp", s.listenAddr)
+	return err
+}
+
+// Serve socks5 proxy server and wait until it returns error.
+func (s *Server) Serve() error {
+	return s.srv.Serve(s.lis)
 }
