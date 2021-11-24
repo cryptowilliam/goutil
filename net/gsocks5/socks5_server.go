d5b6230000ac5f37c6651afe41d37e5437b8d159
cryptowilliam <cryptowilliam@hotmail.com>
2021-11-24 14:26:02 +0800

    add SetCustomDialer func

--- net/gsocks5/socks5_server.go
+++ net/gsocks5/socks5_server.go
@@ -1,22 +1,44 @@
 package gsocks5
 
 import (
+	"github.com/cryptowilliam/goutil/net/gnet"
 	socks5internal "github.com/cryptowilliam/goutil/net/gsocks5/socks5internal"
 )
+
+type (
+	Server struct {
+		listenAddr string
+		dialer     gnet.DialWithCtxFunc
+	}
+)
+
+// NewServer create new socks5 server.
+func NewServer(listenAddr string) *Server {
+	return &Server{listenAddr: listenAddr}
+}
+
+// SetCustomDialer set custom dialer for requests.
+// This operation is optional.
+func (s *Server) SetCustomDialer(dialer gnet.DialWithCtxFunc) {
+	s.dialer = dialer
+}
 
 // ListenAndServe start a tcp socks5 proxy server.
 // For now, non-tcp socks5 proxy server is not necessary, so there is no "network" param.
 // listenAddr example: "127.0.0.1:8000"
-func ListenAndServe(listenAddr string) error {
+func (s *Server) ListenAndServe() error {
 	// Create a SOCKS5 server
 	conf := &socks5internal.Config{}
+	if s.dialer != nil {
+		conf.Dial = s.dialer
+	}
 	server, err := socks5internal.New(conf)
 	if err != nil {
 		return err
 	}
 
 	// Create SOCKS5 proxy on localhost port 8000
-	if err := server.ListenAndServe("tcp", listenAddr); err != nil {
+	if err := server.ListenAndServe("tcp", s.listenAddr); err != nil {
 		return err
 	}
 	return nil
