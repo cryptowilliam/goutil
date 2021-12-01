f174e29beaec13b888c5d0e60ad4767753b1b544
cryptowilliam <cryptowilliam@hotmail.com>
2021-12-01 15:10:43 +0800

    add ListenAny

--- net/gnet/net.go
+++ net/gnet/net.go
@@ -24,18 +24,26 @@ func Dial(network, address string) (net.Conn, error) {
 	}
 }
 
-// Listen multiple protocols.
-func Listen(network, address string) (net.Listener, error) {
+// ListenCop listens multiple connection-oriented protocols.
+func ListenCop(network, address string) (net.Listener, error) {
 	switch strings.ToLower(network) {
-	case "tcp":
-		return net.Listen("tcp", address)
-	case "udp":
-		return net.Listen("udp", address)
+	case "tcp", "tcp4", "tcp6":
+		return net.Listen(network, address)
 	case "kcp":
 		return gkcp.Listen(address)
 	case "quic":
 		return gquic.Listen(address)
 	default:
 		return nil, gerrors.New("unsupported network %s", network)
+	}
+}
+
+// ListenAny listens any supported protocols.
+func ListenAny(network, address string) (net.Listener, error) {
+	switch strings.ToLower(network) {
+	case "udp", "udp4", "udp6", "unixgram", "ip:1", "ip:icmp":
+		return ListenPop(network, address)
+	default:
+		return ListenCop(network, address)
 	}
 }
