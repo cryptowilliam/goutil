62fc2c0380f9d55bec06622bf21a1cea19927bea
cryptowilliam <cryptowilliam@hotmail.com>
2021-12-07 15:48:41 +0800

    Update cidr.go

--- net/gnet/cidr.go
+++ net/gnet/cidr.go
@@ -2,8 +2,28 @@ package gnet
 
 import (
 	"github.com/cryptowilliam/goutil/basic/gerrors"
+	"github.com/yl2chen/cidranger"
 	"net"
 )
+
+type (
+	// CidrRanger is a fast IP to CIDR lookup.
+	CidrRanger struct {
+		rg cidranger.Ranger
+	}
+)
+
+func NewCidrRanger() *CidrRanger {
+	return &CidrRanger{rg: cidranger.NewPCTrieRanger()}
+}
+
+func (cr *CidrRanger) Insert(in net.IPNet) error {
+	return cr.rg.Insert(cidranger.NewBasicRangerEntry(in))
+}
+
+func (cr *CidrRanger) Contains(ip net.IP) (bool, error) {
+	return cr.rg.Contains(ip)
+}
 
 // Extracts IP mask from CIDR address.
 func CIDRToMask(cidr string) (net.IPMask, error) {
