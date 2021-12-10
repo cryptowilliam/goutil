6f8a31d1e3f677e161d6a490ae7f941a3c6a51cc
cryptowilliam <cryptowilliam@hotmail.com>
2021-12-10 16:00:15 +0800

    Update debug.go

--- basic/gdebug/debug.go
+++ basic/gdebug/debug.go
@@ -2,7 +2,6 @@ package gdebug
 
 import (
 	"net/http"
-	"net/http/pprof"
 	"time"
 )
 
@@ -11,13 +10,6 @@ func ListenAndServe(listen string, duration time.Duration) error {
 
 	// basic stats
 	r.HandleFunc("/stats", statsHandler)
-
-	// text profile
-	r.HandleFunc("/pprof/", pprof.Index)
-	r.HandleFunc("/pprof/cmdline", pprof.Cmdline)
-	r.HandleFunc("/pprof/profile", pprof.Profile)
-	r.HandleFunc("/pprof/symbol", pprof.Symbol)
-	r.HandleFunc("/pprof/trace", pprof.Trace)
 
 	// index page
 	http.Handle("/", r)
