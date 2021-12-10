84b00ce01ce857830a6d91fea226d8bf58a41ac6
cryptowilliam <cryptowilliam@hotmail.com>
2021-12-10 16:03:44 +0800

    Update debug.go

--- basic/gdebug/debug.go
+++ basic/gdebug/debug.go
@@ -2,6 +2,7 @@ package gdebug
 
 import (
 	"net/http"
+	"net/http/pprof"
 	"time"
 )
 
@@ -9,7 +10,14 @@ func ListenAndServe(listen string, duration time.Duration) error {
 	r := http.NewServeMux()
 
 	// basic stats
-	r.HandleFunc("/stats", statsHandler)
+	r.HandleFunc("/debug/stats", statsHandler)
+
+	// text profile
+	r.HandleFunc("/debug/pprof/", pprof.Index)
+	r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	r.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	r.HandleFunc("/debug/pprof/trace", pprof.Trace)
 
 	// index page
 	http.Handle("/", r)
