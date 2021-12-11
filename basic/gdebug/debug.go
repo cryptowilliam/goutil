4aed4fc44a682069d6395fc1dc0bfa8422a9153a
cryptowilliam <cryptowilliam@hotmail.com>
2021-12-11 14:36:45 +0800

    Update debug.go

--- basic/gdebug/debug.go
+++ basic/gdebug/debug.go
@@ -7,53 +7,58 @@ import (
 	"net/http/pprof"
 )
 
-var (
-	pathToBasicStats              = "/debug/stats"
-	pathToTextPprofIndex          = "/debug/pprof/"
-	pathToTextPprofCmdline        = "/debug/pprof/cmdline"
-	pathToTextPprofProfile        = "/debug/pprof/profile"
-	pathToTextPprofSymbol         = "/debug/pprof/symbol"
-	pathToTextPprofTrace          = "/debug/pprof/trace"
-	pathToVisualPprofCPU          = "/debug/visual-pprof/" + profileCPU.String()
-	pathToVisualPprofHeap         = "/debug/visual-pprof/" + profileHeap.String()
-	pathToVisualPprofBlock        = "/debug/visual-pprof/" + profileBlock.String()
-	pathToVisualPprofMutex        = "/debug/visual-pprof/" + profileMutex.String()
-	pathToVisualPprofAllocs       = "/debug/visual-pprof/" + profileAllocs.String()
-	pathToVisualPprofGoRoutine    = "/debug/visual-pprof/" + profileGoRoutine.String()
-	pathToVisualPprofThreadCreate = "/debug/visual-pprof/" + profileThreadCreate.String()
-)
-
-func serveIndexPage(w http.ResponseWriter, r *http.Request) {
-	var v = struct {
+type (
+	allPaths struct {
 		BasicStats              string
 		TextPprofIndex          string
+		TextPprofAllocs         string
+		TextPprofBlock          string
 		TextPprofCmdline        string
+		TextPprofGoroutine      string
+		TextPprofHeap           string
+		TextPprofMutex          string
 		TextPprofProfile        string
-		TextPprofSymbol         string
+		TextPprofThreadCreate   string
 		TextPprofTrace          string
+		TextPprofSymbol         string
+		FullGoroutineStackDump  string
 		VisualPprofCPU          string
 		VisualPprofHeap         string
 		VisualPprofBlock        string
 		VisualPprofMutex        string
 		VisualPprofAllocs       string
-		VisualPprofGoRoutine    string
+		VisualPprofGoroutine    string
 		VisualPprofThreadCreate string
-	}{
-		BasicStats:              pathToBasicStats,
-		TextPprofIndex:          pathToTextPprofIndex,
-		TextPprofCmdline:        pathToTextPprofCmdline,
-		TextPprofProfile:        pathToTextPprofProfile,
-		TextPprofSymbol:         pathToTextPprofSymbol,
-		TextPprofTrace:          pathToTextPprofTrace,
-		VisualPprofCPU:          pathToVisualPprofCPU,
-		VisualPprofHeap:         pathToVisualPprofHeap,
-		VisualPprofBlock:        pathToVisualPprofBlock,
-		VisualPprofMutex:        pathToVisualPprofMutex,
-		VisualPprofAllocs:       pathToVisualPprofAllocs,
-		VisualPprofGoRoutine:    pathToVisualPprofGoRoutine,
-		VisualPprofThreadCreate: pathToVisualPprofThreadCreate,
 	}
-	if err := indexPageTmpl.Execute(w, &v); err != nil {
+)
+
+var (
+	aps = allPaths{
+		BasicStats:              "/debug/stats",
+		TextPprofIndex:          "/debug/pprof/",
+		TextPprofAllocs:         "/debug/pprof/allocs?debug=1",
+		TextPprofBlock:          "/debug/pprof/block?debug=1",
+		TextPprofCmdline:        "/debug/pprof/cmdline?debug=1",
+		TextPprofGoroutine:      "/debug/pprof/goroutine?debug=1",
+		TextPprofHeap:           "/debug/pprof/heap?debug=1",
+		TextPprofMutex:          "/debug/pprof/mutex?debug=1",
+		TextPprofProfile:        "/debug/pprof/profile?debug=1",
+		TextPprofThreadCreate:   "/debug/pprof/threadcreate?debug=1",
+		TextPprofTrace:          "/debug/pprof/trace?debug=1",
+		TextPprofSymbol:         "/debug/pprof/symbol?debug=1",
+		FullGoroutineStackDump:  "/debug/pprof/goroutine?debug=2",
+		VisualPprofCPU:          "/debug/visual-pprof/" + profileCPU.String(),
+		VisualPprofHeap:         "/debug/visual-pprof/" + profileHeap.String(),
+		VisualPprofBlock:        "/debug/visual-pprof/" + profileBlock.String(),
+		VisualPprofMutex:        "/debug/visual-pprof/" + profileMutex.String(),
+		VisualPprofAllocs:       "/debug/visual-pprof/" + profileAllocs.String(),
+		VisualPprofGoroutine:    "/debug/visual-pprof/" + profileGoRoutine.String(),
+		VisualPprofThreadCreate: "/debug/visual-pprof/" + profileThreadCreate.String(),
+	}
+)
+
+func serveIndexPage(w http.ResponseWriter, r *http.Request) {
+	if err := indexPageTmpl.Execute(w, &aps); err != nil {
 		glog.Erro(err, "tmpl.Execute")
 	}
 }
@@ -65,27 +70,27 @@ func ListenAndServe(listen string) error {
 	r.HandleFunc("/", serveIndexPage)
 
 	// basic stats
-	r.HandleFunc(pathToBasicStats, statsHandler)
+	r.HandleFunc(aps.BasicStats, statsHandler)
 
 	// text profile
-	r.HandleFunc(pathToTextPprofIndex, pprof.Index)
-	r.HandleFunc(pathToTextPprofCmdline, pprof.Cmdline)
-	r.HandleFunc(pathToTextPprofProfile, pprof.Profile)
-	r.HandleFunc(pathToTextPprofSymbol, pprof.Symbol)
-	r.HandleFunc(pathToTextPprofTrace, pprof.Trace)
+	r.HandleFunc(aps.TextPprofIndex, pprof.Index)
+	r.HandleFunc(aps.TextPprofCmdline, pprof.Cmdline)
+	r.HandleFunc(aps.TextPprofProfile, pprof.Profile)
+	r.HandleFunc(aps.TextPprofSymbol, pprof.Symbol)
+	r.HandleFunc(aps.TextPprofTrace, pprof.Trace)
 
 	// visual profile
 	vp, err := newVisualizePprof(glog.DefaultLogger)
 	if err != nil {
 		return err
 	}
-	r.HandleFunc(pathToVisualPprofCPU, vp.serveVisualPprof)
-	r.HandleFunc(pathToVisualPprofHeap, vp.serveVisualPprof)
-	r.HandleFunc(pathToVisualPprofBlock, vp.serveVisualPprof)
-	r.HandleFunc(pathToVisualPprofMutex, vp.serveVisualPprof)
-	r.HandleFunc(pathToVisualPprofAllocs, vp.serveVisualPprof)
-	r.HandleFunc(pathToVisualPprofGoRoutine, vp.serveVisualPprof)
-	r.HandleFunc(pathToVisualPprofThreadCreate, vp.serveVisualPprof)
+	r.HandleFunc(aps.VisualPprofCPU, vp.serveVisualPprof)
+	r.HandleFunc(aps.VisualPprofHeap, vp.serveVisualPprof)
+	r.HandleFunc(aps.VisualPprofBlock, vp.serveVisualPprof)
+	r.HandleFunc(aps.VisualPprofMutex, vp.serveVisualPprof)
+	r.HandleFunc(aps.VisualPprofAllocs, vp.serveVisualPprof)
+	r.HandleFunc(aps.VisualPprofGoroutine, vp.serveVisualPprof)
+	r.HandleFunc(aps.VisualPprofThreadCreate, vp.serveVisualPprof)
 
 	// index page
 	http.Handle("/", r)
@@ -101,19 +106,25 @@ var indexPageTmpl = template.Must(template.New("").Parse(`<!DOCTYPE html>
   </head>
   <body>
 	<div style="width:95%; height:95%; display: inline-block; vertical-align: top;">
-		<p><a href="{{.BasicStats}}" target="_blank">Basic stats information -> {{.BasicStats}}</a></p>
-		<p><a href="{{.TextPprofIndex}}" target="_blank">Text pprof index -> {{.TextPprofIndex}}</a></p>
-		<p><a href="{{.TextPprofCmdline}}" target="_blank">Text pprof cmdline -> {{.TextPprofCmdline}}</a></p>
-		<p><a href="{{.TextPprofProfile}}" target="_blank">Text pprof profile -> {{.TextPprofProfile}}</a></p>
-		<p><a href="{{.TextPprofSymbol}}" target="_blank">Text pprof symbol -> {{.TextPprofSymbol}}</a></p>
-		<p><a href="{{.TextPprofTrace}}" target="_blank">Text pprof trace -> {{.TextPprofTrace}}</a></p>
-		<p><a href="{{.VisualPprofCPU}}" target="_blank">Visual pprof CPU (wait 10+ seconds) -> {{.VisualPprofCPU}}</a></p>
-		<p><a href="{{.VisualPprofHeap}}" target="_blank">Visual pprof heap -> {{.VisualPprofHeap}}</a></p>
-		<p><a href="{{.VisualPprofBlock}}" target="_blank">Visual pprof block -> {{.VisualPprofBlock}}</a></p>
-		<p><a href="{{.VisualPprofMutex}}" target="_blank">Visual pprof mutex -> {{.VisualPprofMutex}}</a></p>
-		<p><a href="{{.VisualPprofAllocs}}" target="_blank">Visual pprof allocs -> {{.VisualPprofAllocs}}</a></p>
-		<p><a href="{{.VisualPprofGoRoutine}}" target="_blank">Visual pprof Go routine -> {{.VisualPprofGoRoutine}}</a></p>
-		<p><a href="{{.VisualPprofThreadCreate}}" target="_blank">Visual pprof thread create -> {{.VisualPprofThreadCreate}}</a></p>
+		<p><a href="{{.BasicStats}}" target="_blank">Basic stats information</a></p>
+		<p><a href="{{.TextPprofIndex}}" target="_blank">Text pprof index</a></p>
+		<p><a href="{{.TextPprofAllocs}}" target="_blank">Text pprof allocs</a></p>
+		<p><a href="{{.TextPprofBlock}}" target="_blank">Text pprof block</a></p>
+		<p><a href="{{.TextPprofCmdline}}" target="_blank">Text pprof cmdline</a></p>
+		<p><a href="{{.TextPprofGoroutine}}" target="_blank">Text pprof goroutine</a></p>
+		<p><a href="{{.TextPprofHeap}}" target="_blank">Text pprof heap</a></p>
+		<p><a href="{{.TextPprofMutex}}" target="_blank">Text pprof mutex</a></p>
+		<p><a href="{{.TextPprofProfile}}" target="_blank">Text pprof profile</a></p>
+		<p><a href="{{.TextPprofThreadCreate}}" target="_blank">Text pprof threadcreate</a></p>
+		<p><a href="{{.TextPprofTrace}}" target="_blank">Text pprof trace</a></p>
+		<p><a href="{{.TextPprofSymbol}}" target="_blank">Text pprof symbol</a></p>
+		<p><a href="{{.VisualPprofCPU}}" target="_blank">Visual pprof CPU (wait 10+ seconds)</a></p>
+		<p><a href="{{.VisualPprofHeap}}" target="_blank">Visual pprof heap</a></p>
+		<p><a href="{{.VisualPprofBlock}}" target="_blank">Visual pprof block</a></p>
+		<p><a href="{{.VisualPprofMutex}}" target="_blank">Visual pprof mutex</a></p>
+		<p><a href="{{.VisualPprofAllocs}}" target="_blank">Visual pprof allocs</a></p>
+		<p><a href="{{.VisualPprofGoRoutine}}" target="_blank">Visual pprof Go routine</a></p>
+		<p><a href="{{.VisualPprofThreadCreate}}" target="_blank">Visual pprof thread create</a></p>
 	</div>
   </body>
 </html>
