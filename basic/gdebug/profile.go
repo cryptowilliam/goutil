e6af187a9b8536c163e28c2eed09e73c9fb4f536
cryptowilliam <cryptowilliam@hotmail.com>
2021-12-13 19:13:45 +0800

    Update profile.go

--- basic/gdebug/profile.go
+++ basic/gdebug/profile.go
@@ -166,7 +166,7 @@ func (p *Profile) ToDotGraph() ([]byte, error) {
 }
 
 // ToSvg convert profile to SVG image.
-// FIXME: output image is totally different from "go tool pprof"
+// FIXME: output image is totally different from "go tool pprof -svg -output imagePath binaryPath profilePath"
 func (p *Profile) ToSvg() ([]byte, error) {
 	buf := bytes.Buffer{}
 	if err := (*profile.Profile)(p).Write(&buf); err != nil {
@@ -192,7 +192,7 @@ func (p *Profile) ToSvg() ([]byte, error) {
 }
 
 // ToPng convert profile to PNG image.
-// FIXME: output image is totally different from "go tool pprof"
+// FIXME: output image is totally different from "go tool pprof -png -output imagePath binaryPath profilePath"
 func (p *Profile) ToPng() ([]byte, error) {
 	dotBuf, err := p.ToDotGraph()
 	if err != nil {
