6f4eaa3083eef597e0459e6530aa875b2e1a95b4
cryptowilliam <cryptowilliam@hotmail.com>
2021-12-18 15:25:40 +0800

    Update svg_server.go

--- basic/gdebug/svg_server.go
+++ basic/gdebug/svg_server.go
@@ -48,7 +48,7 @@ func (s *svgServer) replyError(w http.ResponseWriter, err error, wrapMsg string)
 
 func (s *svgServer) serve(w http.ResponseWriter, r *http.Request) {
 	// FIXME: I'm not sure if this is right
-	defer r.Response.Body.Close()
+	defer r.Body.Close()
 
 	s.log.Infof("accept visual pprof request %s", r.URL.String())
 	ss := strings.Split(r.URL.Path, "debug/visual-pprof/")
